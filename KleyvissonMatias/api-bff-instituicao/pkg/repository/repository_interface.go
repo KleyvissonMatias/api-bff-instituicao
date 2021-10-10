c9525a972eea94827b3788f0f0084896d2e6cafb
Kley <kleyvissonmatias@gmail.com>
2021-10-10 19:15:14 -0300

    Delete KleyvissonMatias/api-bff-instituicao directory

--- KleyvissonMatias/api-bff-instituicao/pkg/repository/repository_interface.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-type MongoRepository interface {
-	GetAll() ([]interface{}, error)
-	GetByID(id string) (interface{}, error)
-	Create(user interface{}) error
-	Delete(id string) error
-	Update(id string, user interface{}) error
-}
